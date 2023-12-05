7d21db7ec2d9938417b961d5a5c75be79e1010aa
Bo Brockmann <bbrockmann1@gmail.com>
2023-12-05 00:19:51 -0700

    dino game

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.gif';
 import './App.css';
+import ChromeDinoGame from 'react-chrome-dino';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} classname={App-logo} alt={logo}/>
+        <img src={logo} alt={logo}/>
         <p>
           Hello, world!
         </p>
+        <div className="game-container">
+          {/* Game renders twice in development due to to React Strict Mode. Will not render twice is production. */}
+          <ChromeDinoGame />
+        </div>         
         <p>
           This website is currently under construction.
         </p>
