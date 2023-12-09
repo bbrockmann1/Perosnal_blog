f808e970d24aa204e06bd74e3323aca0430adf94
Bo Brockmann <bbrockmann1@gmail.com>
2023-12-09 16:06:23 -0700

    added about and links buttons and styling

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './header.css';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
+  function handleDropdown() {
+    setOpen(!open);
+  };
+
   return (
-    <div className='border'>
-      <header className='home-header'>
-        <h2>Robert Brockmann</h2>
-        <h1>
-          <span>Tech and Web Dev</span>
-        </h1>
-        <p>
-          A place for my interests in tech. <br /> Web development, security, Raspberry Pi, IT and programming.
-        </p>
-      </header>
-    </div>
-    
+      <>
+        <header className='home-header'>
+          <h2>Robert Brockmann</h2>
+          <h1>
+            <span>Tech and Web Dev</span>
+          </h1>
+          <p>
+            A place for my interests in tech. <br /> Web development, security, Raspberry Pi, IT and programming.
+          </p>
+        </header>
+        <div className='links-container'>
+          <Link to="/about" className="about-button">
+            About
+          </Link>          
+            <button className="toggle-button" onClick={handleDropdown}>Links</button>
+
+        </div>
+      </>
   );
 };
 
