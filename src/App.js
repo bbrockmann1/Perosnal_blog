004fc4d65951a394978c1fd2f336171416c69d3f
Bo Brockmann <bbrockmann1@gmail.com>
2023-12-09 14:57:18 -0700

    added about page and routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blog from './pages/Blog/Blog';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
     <div className='container'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/' element={ <Home /> } />
+        <Route path='/blog/:id' element={ <Blog /> } />
+        <Route path='/about' element={ <About /> }/>
       </Routes>
     </div>
   );
