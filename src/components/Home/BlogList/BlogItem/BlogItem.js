72d881eccf48d744b7fad98fdc0ef12eb0c1308d
Bo Brockmann <bbrockmann1@gmail.com>
2023-12-10 18:43:21 -0700

    blog page renders images and text dynamically

--- src/components/Home/BlogList/BlogItem/BlogItem.js
+++ src/components/Home/BlogList/BlogItem/BlogItem.js
@@ -3,33 +3,26 @@ import { Link } from 'react-router-dom';
 import Chip from '../../../common/Chip/Chip';
 import './blogItem.css';
 
-function BlogItem({ 
-  blog: {
-    description,
-    title,
-    createdAt,
-    cover,
-    category,
-    id,
-  }
- }) {
+function BlogItem({ blog: { id, title, category, createdAt, cover, description } }) {
+  const firstText = description.find(desc => desc.type === 'text');
+
   return (
-  <Link className='blogItem-link' to={`/blog/${id}`}>
-    <div className='blogItem-wrap'>
-      <img className='blogItem-cover' src={cover} alt='cover' />
-      <Chip label={category} />
-      <h3 className='title'>{title}</h3>
-      <p className='blogItem-desc'>{description.value.substring(0, 90) + '...'}</p>
-      <footer>
-        <div className='blogItem-author'>
-          <div>
-            <p>{createdAt}</p>
+    <Link className='blogItem-link' to={`/blog/${id}`}>
+      <div className='blogItem-wrap'>
+        <img className='blogItem-cover' src={cover} alt='cover' />
+        <Chip label={category} />
+        <h3 className='title'>{title}</h3>
+        {firstText && <p className='blogItem-desc'>{firstText.value.substring(0, 60) + '...'}</p>}
+        <footer>
+          <div className='blogItem-author'>
+            <div>
+              <p>{createdAt}</p>
+            </div>
           </div>
-        </div>
-      </footer>
-    </div>
-  </Link>
+        </footer>
+      </div>
+    </Link>
   );
-};
+}
 
 export default BlogItem;
