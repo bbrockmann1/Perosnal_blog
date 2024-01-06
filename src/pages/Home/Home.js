325fff830db115715eea3073fad862398b55e288
Bo Brockmann <bbrockmann1@gmail.com>
2024-01-05 19:29:59 -0700

    loading spinner added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import './home.css';
 import EmptyList from '../../components/common/EmptyList/EmptyList';
 import BlogList from '../../components/Home/BlogList/BlogList';
 import Header from '../../components/Home/Header/Header';
 import SearchBar from '../../components/Home/SearchBar/SearchBar';
+import { FadeLoader } from 'react-spinners';
 
 function Home() {
   const [originalBlogs, setOriginalBlogs] = useState([]);
   const [blogs, setBlogs] = useState([]);
   const [searchKey, setSearchKey] = useState('');
+  const [isLoading, setIsLoading] = useState(true); // Loading state
 
   useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  const fetchBlogs = () => {
     fetch('http://localhost:4000/blogs') // Replace with your server URL
       .then((res) => res.json())
       .then((data) => {
         setOriginalBlogs(data);
         setBlogs(data);
+        setIsLoading(false); // Update loading state once data is fetched
       })
-      .catch((error) => console.error('Error fetching blogs:', error));
-  }, []);
+      .catch((error) => {
+        console.error('Error fetching blogs:', error);
+        setIsLoading(false); // Handle error by updating loading state
+      });
+  };
 
   const handleSearchBar = (e) => {
     e.preventDefault();
@@ -45,7 +56,20 @@ function Home() {
         formSubmit={handleSearchBar}
         handleSearchKey={(e) => setSearchKey(e.target.value)}
       />
-      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+      {isLoading ? (
+        <div className='loader-container'>
+          <FadeLoader
+            color="#FFFFFF"
+            loading={isLoading}
+            height={15}
+            width={5} 
+            radius={2} 
+            margin={2}
+          />
+        </div>
+      ) : (
+        !blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />
+      )}
     </div>
   );
 }
